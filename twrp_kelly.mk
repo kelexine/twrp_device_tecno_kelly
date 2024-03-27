e84116329c5f9a75c80e2fc24424f1e2585da7b6
kelexine <frankiekelechi@gmail.com>
2024-03-27 19:24:17 +0100

    Revert "improve"
    This reverts commit 899dfba769a2b3e3eb5cd75e375e0a119cacc1de.

--- twrp_kelly.mk
+++ twrp_kelly.mk
@@ -4,11 +4,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from kelly device
@@ -17,4 +20,10 @@ $(call inherit-product, device/tecno/kelly/device.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-#
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kelly
+PRODUCT_NAME := twrp_kelly
+PRODUCT_BRAND := tecno
+PRODUCT_MODEL := kelly
+PRODUCT_MANUFACTURER := Tecno Mobility Limited
+PRODUCT_RELEASE_NAME := Tecno Spark 8C (kelly)
