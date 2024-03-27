5cb47c26186c40cfe889e6a5ea43129a51495756
kelexine <frankiekelechi@gmail.com>
2024-03-27 19:24:18 +0100

    Revert "improve"
    This reverts commit 25f2e7bc2041ae1902d4ff56bde1bf66010436fa.

--- Android.mk
+++ Android.mk
@@ -7,8 +7,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Define the devices that are supported by this makefile
-SUPPORTED_DEVICES := kelly
-
-# Include all the makefiles for the supported devices
-include $(call all-subdir-makefiles,$(foreach device,$(SUPPORTED_DEVICES),$(LOCAL_PATH)/$(device)))
+ifeq ($(TARGET_DEVICE),kelly)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
